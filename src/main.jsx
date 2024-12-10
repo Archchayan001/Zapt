37ae8ae7696204c4bf3d7fed6a77ad0f941c5579
Archchayan001 <archa13001@gmail.com>
2024-12-10 19:32:54 +0530

    Initial commit2

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import App from './App.jsx';  // Assuming you have a JSX file for your App component
+import './index.css';         // Assuming you have CSS for styling
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
